e1d5c83104f879bff1f088e9fbc91def696c88b7
marinazanoni <146860952+marinazanoni@users.noreply.github.com>
2023-10-15 15:25:39 +0200

    Update submissionHM1-15-10-Zanoni.py

--- submissionHM1-15-10-Zanoni.py
+++ submissionHM1-15-10-Zanoni.py
@@ -1181,9 +1181,7 @@ def fibonacci(n):
         return([0])
     res=[0,1]
     for i in range(2,n):
-        #print(i, res[i-2], res[i-1], res[i-1]+res[i-2])
         res.append(res[i-1]+res[i-2])
-        #print(res)
     return(res)
 
 if __name__ == '__main__':
